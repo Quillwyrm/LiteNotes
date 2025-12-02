f2a17040fa323b1d6749b24424d561cbc845c917
Jacob Holland <91057576+Quillwyrm@users.noreply.github.com>
2025-12-02 15:23:33 +1100

    Update config.lua

--- litemark/config.lua
+++ litemark/config.lua
@@ -4,7 +4,6 @@ local plugin_dir = common.dirname(debug.getinfo(1, "S").source:sub(2))
 
 return {
   singleton_notes = true,
-  notes_panel_size = 256,
   dock_mode = "right", 
   header_rules = true, -- Draw a thin rule under H1 / H2 headings
   
